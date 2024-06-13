e36f394daa78cf4da8c569b7a0345d940bd35c0b
Aditya Kumar <84377163+thakuradi@users.noreply.github.com>
2024-06-13 17:26:11 +0530

    added db

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+// mongodb+srv://thakur:7X%403uw8r@cluster0.ai2rpio.mongodb.net/
+mongoose.connect(' mongodb+srv://thakur:7X%403uw8r@cluster0.ai2rpio.mongodb.net')
+const todoSchema = mongoose.Schema({
+    title:"string",
+    description:"string",
+    completed:"Boolean"
+})
+const todo = mongoose.model('todos',todoSchema)
+module.exports={
+    todo
+}
